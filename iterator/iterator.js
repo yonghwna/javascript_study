b5ad135899c64ccbc76a43fd3e4690de053eec07
yonghwna <yongdev0273@gmail.com>
2024-08-29 12:36:16 +0900

    iterator

---
+++ iterator/iterator.js
@@ -0,0 +1,29 @@
+let range = {
+  from: 1,
+  to: 5,
+};
+
+/**이걸 이터러블로 만드려면 객체에 Symbol.iterator를 추가해야함.
+ * Symbol.iterator는 반드시 이터레이터(next메서드가 있는 객체)를 반환해야함.
+ *
+ */
+
+// 1. for..of 최초 호출 시, Symbol.iterator가 호출됩니다.
+range[Symbol.iterator] = function () {
+  // Symbol.iterator는 이터레이터 객체를 반환합니다.
+  // 2. 이후 for..of는 반환된 이터레이터 객체만을 대상으로 동작하는데, 이때 다음 값도 정해집니다.
+  return {
+    current: this.from,
+    last: this.to,
+
+    // 3. for..of 반복문에 의해 반복마다 next()가 호출됩니다.
+    next() {
+      // 4. next()는 값을 객체 {done:.., value :...}형태로 반환해야 합니다.
+      if (this.current <= this.last) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
